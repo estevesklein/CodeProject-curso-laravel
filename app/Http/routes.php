ef7e5012f591d5090b595085607e985ddfacbe2a
Esteves Klein <esteves.klein@gmail.com>
2015-07-21 14:51:55 -0300

    Correção do verbo put para action update

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -20,4 +20,4 @@ Route::get('client', 'ClientController@index');
 Route::post('client', 'ClientController@create');
 Route::get('client/{id}', 'ClientController@show');
 Route::delete('client/{id}', 'ClientController@destroy');
-Route::post('client/{id}', 'ClientController@update');
+Route::put('client/{id}', 'ClientController@update');
