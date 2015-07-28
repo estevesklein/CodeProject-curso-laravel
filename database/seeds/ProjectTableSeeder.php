8ad4b2d6ad91805951ace6ee4ada081c4cd10779
Esteves Klein <esteves.klein@gmail.com>
2015-07-28 18:36:32 -0300

    Atualizações para Seeder Project

---
+++ database/seeds/ProjectTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ProjectTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // apagar todos os registro da tabela
+        \CodeProject\Entities\Project::truncate();
+        // criar 10 registros na tabela
+        factory(\CodeProject\Entities\Project::class, 10)->create();
+    }
+}
