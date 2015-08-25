52e902d1fa467bb10a4e8f1238a13013ec83097e
Esteves Klein <esteves.klein@gmail.com>
2015-08-25 18:04:34 -0300

    Implementando a atividade: Configurando o ambiente de desenvolvimento

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,7 @@ config.bower_path = config.assets_path + '/../bower_components';
 
 config.build_path_js = config.build_path + '/js';
 config.build_vendor_path_js = config.build_path_js + '/vendor';
+
 config.vendor_path_js = [
 	config.bower_path + '/jquery/dist/jquery.min.js',
 	config.bower_path + '/bootstrap/dist/js/bootstrap.min.js',
@@ -22,8 +23,8 @@ config.vendor_path_js = [
 	config.bower_path + '/angular-resource/angular-resource.min.js',
 	config.bower_path + '/angular-animate/angular-animate.min.js',
 	config.bower_path + '/angular-messages/angular-messages.min.js',
-	config.bower_path + '/angular-bootstrap/ui-boostrap.min.js',
-	config.bower_path + '/angular-strap/modules/navbar.min.js'
+	config.bower_path + '/angular-bootstrap/ui-bootstrap.min.js',
+	config.bower_path + '/angular-strap/dist/modules/navbar.min.js'
 ];
 
 config.build_path_css = config.build_path + '/css';
@@ -41,6 +42,7 @@ gulp.task('teste', function(){
 */
 
 gulp.task('copy-styles', function(){
+
 	gulp.src([
 		config.assets_path + '/css/**/*.css'
 	])
@@ -77,17 +79,16 @@ gulp.task('watch-dev', ['clear-build-folder'], function(){
 	gulp.watch(config.assets_path + '/**', ['copy-styles', 'copy-scripts']);
 });
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
 
-elixir(function(mix) {
-    mix.sass('app.scss');
+// 25.08.2015 - Default //
+gulp.task('default',['clear-build-folder'], function(){
+	elixir(function(mix) {
+    	//mix.sass('app.scss');
+    	mix.styles(config.vendor_path_css.concat([config.assets_path + '/css/**/*.css']), 'public/css/all.css', config.assets_path);
+    	mix.scripts(config.vendor_path_js.concat([config.assets_path + '/js/**/*.js']), 'public/js/all.js', config.assets_path);
+
+    	mix.version(['js/all.js', 'css/all.css']);
+	});
+
 });
+
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,7 +7,7 @@
 	<title>Laravel</title>
 
 	@if(Config::get('app.debug'))
-		<link href="{{ asset('build/css/vendor/bootstrap.css') }}" rel="stylesheet"/>
+		<link href="{{ asset('build/css/vendor/bootstrap.min.css') }}" rel="stylesheet"/>
 		<link href="{{ asset('build/css/vendor/bootstrap-theme.min.css') }}" rel="stylesheet"/>
 	@else
 		<link href="{{ elixir('css/all.css') }}" rel="stylesheet"/>
@@ -63,17 +63,17 @@
 	@yield('content')
 
 	<!-- Scripts -->
-	@if(Config::get('app.debug'))
-		<script src="{{ assets('build/js/vendor/jquery.min.js') }}"></script>
-		<script src="{{ assets('build/js/vendor/angular.min.js') }}"></script>
-		<script src="{{ assets('build/js/vendor/angular-route.min.js') }}"></script>
-		<script src="{{ assets('build/js/vendor/angular-resource.min.js') }}"></script>
-		<script src="{{ assets('build/js/vendor/angular-animate.min.js') }}"></script>
-		<script src="{{ assets('build/js/vendor/angular-messages.min.js') }}"></script>
-		<script src="{{ assets('build/js/vendor/ui-bootstrap.min.js') }}"></script>
-		<script src="{{ assets('build/js/vendor/navbar.min.js') }}"></script>
-	@else
-		<script src="{{ elixir('js/all.js') }}"></script>
-	@endif
+@if(Config::get('app.debug'))
+<script src="{{ asset('build/js/vendor/jquery.min.js') }}"></script>
+<script src="{{ asset('build/js/vendor/angular.min.js') }}"></script>
+<script src="{{ asset('build/js/vendor/angular-route.min.js') }}"></script>
+<script src="{{ asset('build/js/vendor/angular-resource.min.js') }}"></script>
+<script src="{{ asset('build/js/vendor/angular-animate.min.js') }}"></script>
+<script src="{{ asset('build/js/vendor/angular-messages.min.js') }}"></script>
+<script src="{{ asset('build/js/vendor/ui-bootstrap.min.js') }}"></script>
+<script src="{{ asset('build/js/vendor/navbar.min.js') }}"></script>
+@else
+<script src="{{ elixir('js/all.js') }}"></script>
+@endif
 </body>
 </html>
