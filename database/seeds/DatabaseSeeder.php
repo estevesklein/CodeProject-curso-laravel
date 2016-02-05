ead39b4a64fe20e944fed510ded0368eb5b59964
Esteves Klein <esteves.klein@gmail.com>
2016-02-04 23:32:18 -0200

    Correção da atividade: Autorização

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -24,6 +24,8 @@ class DatabaseSeeder extends Seeder
         $this->call(ProjectNoteTableSeeder::class);
         $this->call(ProjectTaskTableSeeder::class);
         $this->call(ProjectMemberTableSeeder::class);
+        $this->call(OauthClientsTableSeeder::class);
+
 
         Model::reguard();
     }
